17268b3172d9b50169fb4e4236fa335b0e53807c
loyti <brice@loyti.com>
2017-06-26 11:46:22 -0700

    updated js file

--- playerDetails/playerDetails.js
+++ playerDetails/playerDetails.js
@@ -11,6 +11,13 @@ var users = {
     ]
 };
 function printAll(){
-     document.write("<p>This student's name is: " + users.Students.first_name[0] + users.Students.last_name[0] + "</p>");
-}
+    document.write("<h1>Meet the Students</h1>");
+    for (var i = 0; i < users.Students.length; i++){
+        document.write("<p>This student's name is: " + users.Students[i].first_name + " " + users.Students[i].last_name + " and the student's name is " + (users.Students[i].first_name.length + users.Students[i].last_name.length) + " characters long.</p>");
+    };
+    document.write("<h1>Meet the Instructors</h1>");
+    for (var j = 0; j < users.Instructors.length; j++){
+        document.write("<p>This Instructor's name is: " + users.Instructors[j].first_name + " " + users.Instructors[j].last_name + " and the instructor's name is " + (users.Instructors[j].first_name.length + users.Instructors[j].last_name.length) + " characters long.</p>");
+    };
+};
 printAll();
